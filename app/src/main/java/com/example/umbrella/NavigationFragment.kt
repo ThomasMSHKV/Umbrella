372e20dce9e916ca83376732f99f9e2526867e01
Thomas Mereshkov <tmereshkov@gmail.com>
2020-08-27 16:00:35 +0300

    Continuation

---
+++ app/src/main/java/com/example/umbrella/NavigationFragment.kt
@@ -0,0 +1,19 @@
+package com.example.umbrella
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class NavigationFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_navigation, container, false)
+    }
+}
